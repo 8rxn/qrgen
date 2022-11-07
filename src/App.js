d43a81024f81ad2341ef4c7cd9e60b93c86e7247
8rxn <rajaryanvishwakarma10@gmail.com>
2022-11-07 19:54:15 +0530

    just making some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Form from "./components/Form";
+import Result from "./components/Result";
+import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
+  
+  const [TextValue, setTextValue] = useState("")
+  const [TypeValue, setTypeValue] = useState("qr")
+  const [EncodingValue, setEncodingValue] = useState("UTF-8")
+  const [HeightValue, setHeightValue] = useState("300")
+  const [WidthValue, setWidthValue] = useState("300")
+  const [Invert, setInvert] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Navbar invert={setInvert}/>
+<div className="d-flex justify-content-around flex-wrap align-items-center m-5">
+  <Form setTextValue={setTextValue} setTypeValue={setTypeValue} setEncodingValue={setEncodingValue} setHeightValue={setHeightValue} setWidthValue={setWidthValue} />
+{TextValue?<Result mode={Invert} text={TextValue} type={TypeValue} encoding={EncodingValue} height={HeightValue} width={WidthValue}/>:""
+}</div>
+<Footer/>
+</>
   );
 }
 
